592f677028fc70c207a63b534bc16c9232047101
Petr Manek <petrmanek@users.noreply.github.com>
2019-02-13 14:59:39 +0100

    cpp: add base_acquisition, move most of general functions

--- cxx/include/katherinexx/acquisition.hpp
+++ cxx/include/katherinexx/acquisition.hpp
@@ -31,6 +31,112 @@ enum class acq_mode: int {
     event_itot  = ACQUISITION_MODE_EVENT_ITOT
 };
 
+class base_acquisition {
+public:
+    using frame_started_handler     = std::function<void(int)>;
+    using frame_ended_handler       = std::function<void(int, bool, const katherine_frame_info_t *)>;
+
+protected:
+    katherine_acquisition_t acq_;
+
+private:
+    acq_mode mode_;
+    bool fast_vco_enabled_;
+
+    frame_started_handler frame_started_handler_;
+    frame_ended_handler frame_ended_handler_;
+
+    static void
+    forward_frame_started(void *user_ctx, int frame_idx)
+    {
+        auto self = reinterpret_cast<base_acquisition*>(user_ctx);
+        self->frame_started_handler_(frame_idx);
+    }
+
+    static void
+    forward_frame_ended(void *user_ctx, int frame_idx, bool completed, const katherine_frame_info_t *info)
+    {
+        auto self = reinterpret_cast<base_acquisition*>(user_ctx);
+        self->frame_ended_handler_(frame_idx, completed, info);
+    }
+
+public:
+    base_acquisition(device& dev, std::size_t md_buffer_size, std::size_t pixel_buffer_size, acq_mode mode, bool fast_vco_enabled)
+        :acq_{},
+         mode_{mode},
+         fast_vco_enabled_{fast_vco_enabled},
+         frame_started_handler_{},
+         frame_ended_handler_{}
+    {
+        int res = katherine_acquisition_init(&acq_, dev.c_dev(), reinterpret_cast<void*>(this), md_buffer_size, pixel_buffer_size);
+        if (res != 0) {
+            throw katherine::system_error{res};
+        }
+
+        /* TODO: uncomment in C++2a */
+        acq_.handlers = {
+            /* .pixels_received = */ nullptr,
+            /* .frame_started = */ base_acquisition::forward_frame_started,
+            /* .frame_ended = */ base_acquisition::forward_frame_ended
+        };
+    }
+
+    virtual ~base_acquisition()
+    {
+        katherine_acquisition_fini(&acq_);
+    }
+
+    void
+    set_frame_started_handler(frame_started_handler&& fn)
+    {
+        frame_started_handler_ = std::move(fn);
+    }
+
+    void
+    set_frame_ended_handler(frame_ended_handler&& fn)
+    {
+        frame_ended_handler_ = std::move(fn);
+    }
+
+    void
+    begin(const katherine::config& config, katherine::readout_type readout_type)
+    {
+        int res = katherine_acquisition_begin(&acq_, config.c_config(), (char) readout_type,
+            (katherine_acquisition_mode_t) mode_, fast_vco_enabled_);
+
+        if (res != 0) {
+            throw katherine::system_error{res};
+        }
+    }
+
+    void
+    abort()
+    {
+        int res = katherine_acquisition_abort(&acq_);
+
+        if (res != 0) {
+            throw katherine::system_error{res};
+        }
+    }
+
+    void
+    read()
+    {
+        int res = katherine_acquisition_read(&acq_);
+
+        if (res != 0) {
+            throw katherine::system_error{res};
+        }
+    }
+
+    acq_state state() const                         { return (acq_state) acq_.state; }
+    int requested_frames() const                    { return acq_.requested_frames; }
+    int completed_frames() const                    { return acq_.completed_frames; }
+    std::size_t dropped_measurement_data() const    { return acq_.dropped_measurement_data; }
+
+};
+
+
 namespace acq {
 
 struct f_toa_tot {
@@ -71,21 +177,15 @@ struct event_itot {
 
 }
 
-template<typename AcqMode>
-class acquisition {
-    katherine_acquisition_t acq_;
 
+template<typename AcqMode>
+class acquisition: public base_acquisition {
 public:
     using pixel_type                = typename AcqMode::pixel_type;
-
     using pixels_received_handler   = std::function<void(const pixel_type *, std::size_t)>;
-    using frame_started_handler     = std::function<void(int)>;
-    using frame_ended_handler       = std::function<void(int, bool, const katherine_frame_info_t *)>;
 
 private:
     pixels_received_handler pixels_received_handler_;
-    frame_started_handler frame_started_handler_;
-    frame_ended_handler frame_ended_handler_;
 
     static void
     forward_pixels_received(void *user_ctx, const void *px, size_t count)
@@ -95,38 +195,12 @@ private:
         self->pixels_received_handler_(derived_px, count);
     }
 
-    static void
-    forward_frame_started(void *user_ctx, int frame_idx)
-    {
-        auto self = reinterpret_cast<acquisition*>(user_ctx);
-        self->frame_started_handler_(frame_idx);
-    }
-
-    static void
-    forward_frame_ended(void *user_ctx, int frame_idx, bool completed, const katherine_frame_info_t *info)
-    {
-        auto self = reinterpret_cast<acquisition*>(user_ctx);
-        self->frame_ended_handler_(frame_idx, completed, info);
-    }
-
 public:
     acquisition(device& dev, std::size_t md_buffer_size, std::size_t pixel_buffer_size)
-        :acq_{},
-         pixels_received_handler_{},
-         frame_started_handler_{},
-         frame_ended_handler_{}
+        :base_acquisition{dev, md_buffer_size, pixel_buffer_size, AcqMode::mode, AcqMode::fast_vco_enabled},
+         pixels_received_handler_{}
     {
-        int res = katherine_acquisition_init(&acq_, dev.c_dev(), reinterpret_cast<void*>(this), md_buffer_size, pixel_buffer_size);
-        if (res != 0) {
-            throw katherine::system_error{res};
-        }
-
-        /* TODO: uncomment in C++2a */
-        acq_.handlers = {
-            /* .pixels_received = */ acquisition::forward_pixels_received,
-            /* .frame_started = */ acquisition::forward_frame_started,
-            /* .frame_ended = */ acquisition::forward_frame_ended
-        };
+        acq_.handlers.pixels_received = acquisition::forward_pixels_received;
     }
 
     virtual ~acquisition()
@@ -139,54 +213,6 @@ public:
     {
         pixels_received_handler_ = std::move(fn);
     }
-
-    void
-    set_frame_started_handler(frame_started_handler&& fn)
-    {
-        frame_started_handler_ = std::move(fn);
-    }
-
-    void
-    set_frame_ended_handler(frame_ended_handler&& fn)
-    {
-        frame_ended_handler_ = std::move(fn);
-    }
-
-    void
-    begin(const katherine::config& config, katherine::readout_type readout_type)
-    {
-        int res = katherine_acquisition_begin(&acq_, config.c_config(), (char) readout_type,
-            (katherine_acquisition_mode_t) AcqMode::mode, AcqMode::fast_vco_enabled);
-
-        if (res != 0) {
-            throw katherine::system_error{res};
-        }
-    }
-
-    void
-    abort()
-    {
-        int res = katherine_acquisition_abort(&acq_);
-
-        if (res != 0) {
-            throw katherine::system_error{res};
-        }
-    }
-
-    void
-    read()
-    {
-        int res = katherine_acquisition_read(&acq_);
-
-        if (res != 0) {
-            throw katherine::system_error{res};
-        }
-    }
-
-    acq_state state() const                         { return (acq_state) acq_.state; }
-    int requested_frames() const                    { return acq_.requested_frames; }
-    int completed_frames() const                    { return acq_.completed_frames; }
-    std::size_t dropped_measurement_data() const    { return acq_.dropped_measurement_data; }
 
 };
 
