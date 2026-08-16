24170663a498180c3eca5ebff3754a531c77d1f1
Antonio Bartalesi <antonio.bartalesi@gmail.com>
2026-08-16 10:42:27 +0200

    c: use a 64-bit shift in MASK so wide fields work on LLP64

--- c/src/bitfields.h
+++ c/src/bitfields.h
@@ -57,7 +57,7 @@
  *   3. _BITS_md_header_type.
  */
 
-#define MASK(n)                             ((1ul << (n)) - 1)
+#define MASK(n)                             ((1ull << (n)) - 1)
 #define EXTRACT(val, bitfield, name)        ((_BITS_##bitfield##_##name##_type) (((val) >> (_BITS_##bitfield##_##name##_start)) & _BITS_##bitfield##_##name##_mask))
 
 #endif
