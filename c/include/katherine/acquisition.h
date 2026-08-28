d9a09884a7e7744105e37cc29d471199ac2654e0
Petr Manek <petrmanek@users.noreply.github.com>
2026-08-28 12:02:53 +0200

    c: document acquisition states and readout modes

--- c/include/katherine/acquisition.h
+++ c/include/katherine/acquisition.h
@@ -96,18 +96,18 @@ typedef struct katherine_acquisition_handlers {
  * not "fix" this enum to match such an implementation; its values already
  * match the wire directly. */
 typedef enum katherine_readout_type {
-    READOUT_SEQUENTIAL  = 0,
-    READOUT_DATA_DRIVEN = 1
+    READOUT_SEQUENTIAL  = 0, ///< Frame-based mode: hits are sequentially read out from the matrix at the end of each frame, potentially resulting in dead time between frames. At most the entire matrix (65k pixels) can be hit.
+    READOUT_DATA_DRIVEN = 1  ///< Data-driven mode: hits are propagated through super-pixels while the measurement is ongoing, causing local (but importantly, not global) dead time. Be careful in noisy or data-intensive environments, this mode can produce a lot of data (up to 40 Mhit/s).
 } katherine_readout_type_t;
 
 KATHERINE_EXPORTED const char *
 katherine_str_readout_type(katherine_readout_type_t type);
 
 typedef enum katherine_acquisition_state {
-    ACQUISITION_NOT_STARTED = 0,
-    ACQUISITION_RUNNING     = 1,
-    ACQUISITION_SUCCEEDED   = 2,
-    ACQUISITION_TIMED_OUT   = 3
+    ACQUISITION_NOT_STARTED = 0, ///< The detector is not sensitive, call katherine_acquisition_begin to start measurement.
+    ACQUISITION_RUNNING     = 1, ///< The detector is sensitive and collecting data, call katherine_acquisition_read() to retrieve its output or katherine_acquisition_{stop,abort}() to interrupt it. Note that for decode_data == false, only katherine_acquisition_abort() is viable.
+    ACQUISITION_SUCCEEDED   = 2, ///< The detector is no longer sensitive, the last measurement data arrived and the measurement concluded in an orderly manner.
+    ACQUISITION_TIMED_OUT   = 3, ///< Communications timeout. Either the detector is still measuring and the data flow was disrupted, or the device was powered off unexpectedly. This is also a common failure mode if decode_data == false, and the user fails to call katherine_acquisition_abort() at the end of the measurement.
 } katherine_acquisition_state_t;
 
 typedef struct katherine_acquisition {
